dedc4b33606e99f993645220a98d3a3d62f6c98d
skadyansna <kadyan@sumits-MacBook-Pro.local>
2015-05-24 16:13:54 -0700

    Initial Commit

---
+++ LocationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationApp
+//
+//  Created by sumit kadyan on 2015-05-24.
+//  Copyright (c) 2015 sumit kadyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
